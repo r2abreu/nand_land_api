7a50fd59766b4ac7b08f6f9e94983be7ea7a3879
Arturo Abreu <r2work@pm.me>
2025-07-31 14:40:16 +0000

    chore: concrete node crypto service contributors

--- src/auth/node-crypto/node-crypto.service.ts
+++ src/auth/node-crypto/node-crypto.service.ts
@@ -1,12 +1,20 @@
 import { Inject, Injectable } from "@nestjs/common";
 import type { Cryptable } from "src/interfaces/cryptable.interface";
 import { RANDOM_BYTES, ENCRYPT } from "./tokens";
+import type { randomBytes } from "crypto";
+
+// TODO: There should be a better way
+type ScryptAsync = (
+  password: string | Buffer,
+  salt: string | Buffer,
+  keylen: number,
+) => Promise<Buffer>;
 
 @Injectable()
 export class NodeCryptoService implements Cryptable {
   constructor(
-    @Inject(RANDOM_BYTES) private nodeRandomBytes: Cryptable["randomBytes"],
-    @Inject(ENCRYPT) private nodeEncrypt: Cryptable["encrypt"],
+    @Inject(RANDOM_BYTES) private nodeRandomBytes: typeof randomBytes,
+    @Inject(ENCRYPT) private nodeEncrypt: ScryptAsync,
   ) {}
 
   randomBytes(size: number): Buffer {
